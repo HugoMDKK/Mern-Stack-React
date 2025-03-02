8703aa23dd0fc2cba065459b0787ab4ee874f0e5
Hugo dos Santos <victorhugo0856@gmail.com>
2025-03-02 18:48:37 -0300

    feat: add foulders

---
+++ src/Datas.js
@@ -0,0 +1,49 @@
+export const news = [
+    {
+        title: "Notícia 1",
+        text: "!lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 10
+    }
+
+    {
+        title: "Notícia 2",
+        text: "!lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 10
+    }
+    
+    {
+        title: "Notícia 3",
+        text: "!lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 10
+    }
+
+    {
+        title: "Notícia 4",
+        text: "!lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 10
+    }
+
+    {
+        title: "Notícia 5",
+        text: "!lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 10
+    }
+
+    {
+        title: "Notícia 6",
+        text: "!lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur.",
+        image: "https://picsum.photos/200/300",
+        likes: 2,
+        comments: 10
+    }
+]
